7facb1471dcda4b08d4b146d167349e2af0c793d
Azamat <azamat.17111711@gmail.com>
2021-11-16 16:16:33 +0600

    change api link

--- js/bundle.js
+++ js/bundle.js
@@ -142,60 +142,58 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function cards() {
-    // Используем классы для создание карточек меню
+	// Используем классы для создание карточек меню
 
-    class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.descr = descr;
-            this.price = price;
-            this.classes = classes;
-            this.parent = document.querySelector(parentSelector);
-            this.transfer = 27;
-            this.changeToUAH(); 
-        }
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 3;
+			this.changeToUAH();
+		}
 
-        changeToUAH() {
-            this.price = this.price * this.transfer; 
-        }
+		changeToUAH() {
+			this.price = this.price * this.transfer;
+		}
 
-        render() {
-            const element = document.createElement('div');
+		render() {
+			const element = document.createElement('div');
 
-            if (this.classes.length === 0) {
-                this.classes = "menu__item";
-                element.classList.add(this.classes);
-            } else {
-                this.classes.forEach(className => element.classList.add(className));
-            }
+			if (this.classes.length === 0) {
+				this.classes = 'menu__item';
+				element.classList.add(this.classes);
+			} else {
+				this.classes.forEach((className) => element.classList.add(className));
+			}
 
-            element.innerHTML = `
+			element.innerHTML = `
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    <div class="menu__item-cost">Price:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> $/day</div>
                 </div>
             `;
-            this.parent.append(element);
-        }
-    }
+			this.parent.append(element);
+		}
+	}
 
-    (0,_services_services__WEBPACK_IMPORTED_MODULE_0__.getResource)('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
-                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
-            });
-        });
-        
-
+	(0,_services_services__WEBPACK_IMPORTED_MODULE_0__.getResource)('https://my-json-server.typicode.com/tawmant/Food/menu').then((data) => {
+		data.forEach(({ img, altimg, title, descr, price }) => {
+			new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+		});
+	});
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (cards);
+
 
 /***/ }),
 
